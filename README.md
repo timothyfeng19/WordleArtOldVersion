1ed7dc1ce0391d6ea5e77f12e2528b3d4b64bc30
Timothy <Timothy@Yans-MacBook-Pro.local>
2026-06-17 09:08:01 +0800

    Archived all old files

---
+++ README.md
@@ -0,0 +1,2 @@
+Old version
+Simple script to run in terminal. Instructions shown when running the program. Uses python and re.
---
+++ word.py
@@ -0,0 +1,72 @@
+import re
+
+print("\nPut 'cancel' at any input to quit the program.")
+
+word_of_day = input("Enter daily wordle answer: ")
+
+while True:
+    if word_of_day == "cancel":
+        quit()
+    if len(word_of_day) != 5 or not bool(re.match('^[abcdefghijklmnopqrstuvwxyz]+$', word_of_day)):
+        print("Invalid word input, needs to be 5 letters no capitals.")
+    else:
+        break
+
+word = []
+word_letters = []
+
+def Check(type, id):
+    if type == "#":
+        if word[id] in word_letters:
+            return True
+        else:
+            return False
+    else:
+        if word[id] not in word_letters:
+            return True
+        else:
+            return False
+    
+while True:
+    while True:
+        art = input("Enter Line here (# for green/yellow, _ for a grey space): ")
+        if art == "cancel":
+            quit()
+        if len(art) != 5 or not bool(re.match('^[#_]+$', art)):
+            print("Invalid line input, only # and _")
+            break
+
+        max_words = input("Maximum amount of words given (no input is 20): ")
+        if max_words == "cancel":
+            quit()
+        if max_words == "":
+            max_words = 20
+        elif not bool(re.match('^[0123456789]+$', max_words)):
+                print("Invalid max input, needs to be a whole number or nothing.")
+                break
+
+        word_letters = set(word_of_day)
+        non_word_letters = set("abcdefghijklmnopqrstuvwxyz") - word_letters
+        valid_words = []
+        
+        with open('words.txt', 'r') as file:
+            words = file.read().splitlines()
+
+        for word in words:
+            if len(set(word)) != 5:
+                continue
+
+            first = word[0] in word_letters
+            second = word[1] in word_letters
+            third = word[2] in word_letters
+            fourth = word[3] in word_letters
+            fifth = word[4] in word_letters
+            passes = 0
+
+            for i in range(5):
+                if Check(art[i], i):
+                    passes += 1
+            if passes == 5:
+                valid_words.append(word)
+                
+        print("Matching words (" + str(len(valid_words[:int(max_words)])) + "/" + str(len(valid_words)) + "):", valid_words[:int(max_words)])
